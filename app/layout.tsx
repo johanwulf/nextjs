428bbb0c1b3d8b420f2f1adca0a0653b8e0b7bd8
johanwulf <johanwulf98@gmail.com>
2023-05-18 09:53:41 +0200

    chore(format): configure prettierrc

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,13 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Test site",
+    title: "Test site",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
